dcefa4e104efa88497575c272573feb8c89594c3
Andre Karlsson <andre@sess.se>
2020-02-07 16:42:05 +0100

    Fix BFOR-02: Real Json and table def.

--- docker/timescaledb/docker-entrypoint-initdb.d/a1_create_dummytables.sh
+++ docker/timescaledb/docker-entrypoint-initdb.d/a1_create_dummytables.sh
@@ -1,16 +1,113 @@
 #!/usr/bin/env bash
 
-psql -U "${POSTGRES_USER}" postgres -c "create table DHT22_Temperature_Data (
-  SensorID text,
-  Date_n_Time TIMESTAMPTZ not null,
-  Temperature DOUBLE PRECISION
-);"
+psql -U "${POSTGRES_USER}" postgres -c "create or replace view mqtt_interface as SELECT NULL::json AS jsondata;"
 
-psql -U "${POSTGRES_USER}" postgres -c "create table DHT22_Humidity_Data (
-  SensorID text,
-  Date_n_Time TIMESTAMPTZ not null,
-  Humidity DOUBLE PRECISION
-);"
+psql -U "${POSTGRES_USER}" postgres -c "CREATE OR REPLACE FUNCTION public.mqtt_interface_ins()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY definer
+AS $function$
+BEGIN
 
-psql -U "${POSTGRES_USER}" postgres -c "select create_hypertable('dht22_humidity_data','date_n_time');"
-psql -U "${POSTGRES_USER}" postgres -c "select create_hypertable('dht22_temperature_data','date_n_time');"
+  insert into mqtt_payload_message_history values
+ (  (new.jsondata::json->>'APITimestamp')::timestamptz,
+    (new.jsondata::json->>'Node')::text,
+    (new.jsondata::json->>'Severity')::bigint,
+    (new.jsondata::json->>'Summary')::text,
+    (new.jsondata::json->>'FirstOccurrence')::timestamptz,
+    (new.jsondata::json->>'LastOccurrence')::timestamptz,
+    (new.jsondata::json->>'Tally')::bigint,
+    (new.jsondata::json->>'Acknowledged')::bigint,
+    (new.jsondata::json->>'Serial')::bigint,
+    (new.jsondata::json->>'Source')::text,
+    (new.jsondata::json->>'SupressEscalate')::bigint ,
+    (new.jsondata::json->>'Bana')::text,
+    (new.jsondata::json->>'IvarObjectID')::bigint,
+    (new.jsondata::json->>'IvarSiteObjectID')::bigint,
+    (new.jsondata::json->>'Agent')::text,
+    (new.jsondata::json->>'TTNumber')::text,
+    (new.jsondata::json->>'TTStatus')::text
+    );
+
+  if ((new.jsondata::json->>'Severity')::bigint)=0 then
+    delete from mqtt_payload_message
+    where serial=(new.jsondata::json->>'Serial')::bigint
+      and source=(new.jsondata::json->>'Source')::text;
+    return NULL;
+  end if;
+
+ insert into mqtt_payload_message values
+ (  (new.jsondata::json->>'APITimestamp')::timestamptz,
+    (new.jsondata::json->>'Node')::text,
+    (new.jsondata::json->>'Severity')::bigint,
+    (new.jsondata::json->>'Summary')::text,
+    (new.jsondata::json->>'FirstOccurrence')::timestamptz,
+    (new.jsondata::json->>'LastOccurrence')::timestamptz,
+    (new.jsondata::json->>'Tally')::bigint,
+    (new.jsondata::json->>'Acknowledged')::bigint,
+    (new.jsondata::json->>'Serial')::bigint,
+    (new.jsondata::json->>'Source')::text,
+    (new.jsondata::json->>'SupressEscalate')::bigint ,
+    (new.jsondata::json->>'Bana')::text,
+    (new.jsondata::json->>'IvarObjectID')::bigint,
+    (new.jsondata::json->>'IvarSiteObjectID')::bigint,
+    (new.jsondata::json->>'Agent')::text,
+    (new.jsondata::json->>'TTNumber')::text,
+    (new.jsondata::json->>'TTStatus')::text
+    )
+    ON CONFLICT(source,serial) DO UPDATE SET
+      apitimestamp=(new.jsondata::json->>'APITimestamp')::timestamptz,
+      node=(new.jsondata::json->>'Node')::text,
+      severity=(new.jsondata::json->>'Severity')::bigint,
+      summary=(new.jsondata::json->>'Summary')::text,
+      firstoccurrence = (new.jsondata::json->>'FirstOccurrence')::timestamptz,
+      lastoccurrence = (new.jsondata::json->>'LastOccurrence')::timestamptz,
+      tally = (new.jsondata::json->>'Tally')::bigint,
+      acknowledged = (new.jsondata::json->>'Acknowledged')::bigint,
+      supressescalate = (new.jsondata::json->>'SupressEscalate')::bigint ,
+      bana = (new.jsondata::json->>'Bana')::text,
+      ivarobjectid = (new.jsondata::json->>'IvarObjectID')::bigint,
+      ivarsiteobjectid = (new.jsondata::json->>'IvarSiteObjectID')::bigint,
+      agent = (new.jsondata::json->>'Agent')::text,
+      ttnumber = (new.jsondata::json->>'TTNumber')::text,
+      ttstatus = (new.jsondata::json->>'TTStatus')::text;
+
+    return NEW;
+END
+$function$;"
+
+psql -U "${POSTGRES_USER}" postgres -c "CREATE TRIGGER "tgmqtt_interface_ins"
+  INSTEAD OF INSERT
+  ON public.mqtt_interface
+  FOR EACH ROW
+  EXECUTE PROCEDURE "mqtt_interface_ins"();"
+
+
+psql -U "${POSTGRES_USER}" postgres -c "create table jsonmall(struct json);"
+
+psql -U "${POSTGRES_USER}" postgres -c "create table mqtt_payload_message as select
+(struct::json->>'APITimestamp')::timestamptz as apitimestamp,
+(struct::json->>'Node')::text as node,
+(struct::json->>'Severity')::bigint as severity,
+(struct::json->>'Summary')::text as summary,
+(struct::json->>'FirstOccurrence')::timestamptz as firstoccurrence,
+(struct::json->>'LastOccurrence')::timestamptz as lastoccurrence,
+(struct::json->>'Tally')::bigint as tally,
+(struct::json->>'Acknowledged')::bigint as acknowledged,
+(struct::json->>'Serial')::bigint as serial,
+(struct::json->>'Source')::text as source,
+(struct::json->>'SupressEscalate')::bigint as supressescalate,
+(struct::json->>'Bana')::text as bana,
+(struct::json->>'IvarObjectID')::bigint as ivarobjectid,
+(struct::json->>'IvarSiteObjectID')::bigint as ivarsiteobjectid,
+(struct::json->>'Agent')::text as agent,
+(struct::json->>'TTNumber')::text as ttnumber,
+(struct::json->>'TTStatus')::text as ttstatus
+from jsonmall;"
+
+
+
+psql -U "${POSTGRES_USER}" postgres -c "create table mqtt_payload_message_history as select * from mqtt_payload_message where 1=0;"
+psql -U "${POSTGRES_USER}" postgres -c "create sequence mqtt_payload_message_history_s start with 1 increment by 1;"
+psql -U "${POSTGRES_USER}" postgres -c "alter table mqtt_payload_message_history add column change_version default bigintnextval('mqtt_payload_message_history_s');"
+psql -U "${POSTGRES_USER}" postgres -c "select create_hypertable('mqtt_payload_message_history','apitimestamp');"
--- sla/mqtt_msg_parser.py
+++ sla/mqtt_msg_parser.py
@@ -1,28 +1,87 @@
 import paho.mqtt.client as mqtt
-from data_saver import sensor_Data_Handler
+import psycopg2
+
+import sys
+from os import getenv
+
+PG_DATABASE = getenv('POSTGRES_DATABASE', 'mqtt')
+PG_HOSTNAME = getenv('POSTGRES_HOSTNAME', 'localhost')
+PG_PORT = getenv('POSTGRES_PORT', '5432')
+PG_USERNAME = getenv('POSTGRES_USER', 'postgres')
+PG_PASSWORD = getenv('POSTGRES_PASSWORD', 'k4km0nster')
 
 # MQTT Settings
-MQTT_Broker = "mosquitto"
+MQTT_Broker = "10.28.29.177"
 MQTT_Port = 1883
 Keep_Alive_Interval = 45
 MQTT_Topic = "#"
 QOS = 1
+DEBUG = True
 
-#Subscribe to all Sensors at Base Topic
+
+def singleton(class_):
+    instances = {}
+
+    def getinstance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+
+    return getinstance
+
+
+@singleton
+class DatabaseManager():
+
+    def __init__(self):
+        self.conn = psycopg2.connect(host=PG_HOSTNAME, user=PG_USERNAME, password=PG_PASSWORD, dbname=PG_DATABASE)
+        self.cur = self.conn.cursor()
+        self.cur.execute("SET application_name TO 'MQTT to Postgres Gateway'");
+
+    def add_del_update_db_record(self, sql_query, args=()):
+
+        # If the SQL statement fails for some reason, do a rollback. Otherwise we will hang with "idle in transaction(aborted)" which is bad, bad, bad. Patrik.
+        if sys.version_info[:3] < (3, 0):
+            try:
+                self.cur.execute(sql_query, args)
+            except Exception, e:
+                self.conn.rollback()
+            else:
+                self.conn.commit()
+            return
+        else:
+            try:
+                self.cur.execute(sql_query, args)
+            except Exception as e:
+                self.conn.rollback()
+            else:
+                self.conn.commit()
+            return
+
+    def __del__(self):
+        self.cur.close()
+        self.conn.close()
+
+
+def Data_Handler(payload, dbg=False):
+    dbObj = DatabaseManager()
+    dbObj.add_del_update_db_record("insert into mqtt_interface (jsondata) values ('%s')" % (payload))
+
+
+# Subscribe to all Sensors at Base Topic
 def on_connect(mosq, obj, rc):
     print(mqttc.subscribe(MQTT_Topic, 0))
 
-#Save Data into DB Table
+
+# Save Data into DB Table
 def on_message(mosq, obj, msg):
-    # This is the Master Call for saving MQTT Data into DB
-    # For details of "sensor_Data_Handler" function please refer "sensor_data_to_db.py"
-    print("MQTT Data Received...")
-    print("MQTT Topic: " + msg.topic)
-    print("Data: " + msg.payload.decode())
-    sensor_Data_Handler(msg.topic, msg.payload.decode())
+    Data_Handler(str(msg.payload.decode()), DEBUG)
+
 
 def on_subscribe(client, userdata, mid, granted_qos):
-    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+    if DEBUG:
+        print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
 
 mqttc = mqtt.Client()
 
@@ -31,27 +90,9 @@ mqttc.on_message = on_message
 mqttc.on_connect = on_connect
 mqttc.on_subscribe = on_subscribe
 
-# Connect
+# Connect to MQTT
 mqttc.connect(MQTT_Broker, int(MQTT_Port), int(Keep_Alive_Interval))
 mqttc.subscribe(MQTT_Topic, QOS)
 
 # Continue the network loop
 mqttc.loop_forever()
-
-
-
-# import paho.mqtt.client as paho
-
-# def on_subscribe(client, userdata, mid, granted_qos):
-#    print("Subscribed: "+str(mid)+" "+str(granted_qos))
-
-# def on_message(client, userdata, msg):
-#    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
-
-# client = paho.Client()
-# client.on_subscribe = on_subscribe
-# client.on_message = on_message
-# client.connect("127.0.0.1", 1883)
-# client.subscribe("#", qos=1)
-
-# client.loop_forever()
